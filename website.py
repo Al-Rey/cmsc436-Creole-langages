193c2fc2160b4cb635961aa2038adb8c18af4f16
Jeremys11 <jeremysingh116@gmail.com>
2022-10-26 14:27:37 -0400

    added generic user input

--- website.py
+++ website.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import os
 
 ##
 ## https://medium.com/geekculture/how-to-make-a-web-map-with-pythons-flask-and-leaflet-9318c73c67c3
+## https://www.askpython.com/python-modules/flask/flask-forms
+##
 ##
 
 
-def read_database():
+def read_database(phrase):
     return 0
 
 app = Flask(__name__)
@@ -21,10 +23,18 @@ app = Flask(__name__)
 #   3. Allow user selection 
 #       3a. Alpha -- select Creole language and display it's location on map 
 @app.route('/')
-@app.route('/index')
+def input():
+    return render_template("input.html")
+
+@app.route('/index', methods = ['POST', 'GET'])
 def root():
 
-    markers=[   
+    ## QUERY DATABASE WITH WORD HERE
+    #location_data = read_database(word)
+
+    word = request.form
+
+    location_data =[   
         {'lat':19.3133,'lon':-81.2546,'tooltip':'Cayman Islands English Cayman Creole'},
         {'lat':18.7357,'lon':-70.1627,'tooltip':'Dominican Republic Spanish Haitian Creole'},
         {'lat':18.9712	,'lon':-72.2852,'tooltip':'Haiti French Haitian Creole'},
@@ -33,4 +43,4 @@ def root():
         {'lat':21.5218,'lon':-77.7812,'tooltip':'Cuba (Haitian Creole Minority Population)'}
     ]
 
-    return render_template("index.html", markers=markers)
+    return render_template("index.html", markers=location_data)
