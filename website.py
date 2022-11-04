eb8345634e81e60350ff12bcbfef5f7ce77d1fc5
Jeremys11 <jeremysingh116@gmail.com>
2022-11-04 15:27:19 -0400

    Random testing

--- website.py
+++ website.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request
 import os
+import json
 
 ##
 ## https://medium.com/geekculture/how-to-make-a-web-map-with-pythons-flask-and-leaflet-9318c73c67c3
@@ -13,10 +14,6 @@ def read_database(phrase):
 
 app = Flask(__name__)
 
-# Probably don't need more than one webpage, probably just need to update this webpage whenever user selects something
-# Non-interactive map
-#   1. Get png of the Caribbean
-#   2. lat-lon points for country capitals and label them with languages
 # Interactive map
 #   1. Display blank map of the Caribbean -- probably will use openstreetmaps
 #   2. Color different countries different colors
@@ -32,15 +29,33 @@ def root():
     ## QUERY DATABASE WITH WORD HERE
     #location_data = read_database(word)
 
-    word = request.form
+    #word = request.form
 
-    location_data =[   
+    #HEX COLORS
+    #RED - #FF0000
+    #BLUE - #0000FF
+    #GREEN - #00FF00
+    #YELLOW - #FFFF00
+
+    f = open("locations.geojson")
+    location_data = json.load(f)
+
+    print(location_data)
+
+    """location_data =[   
         {'lat':19.3133,'lon':-81.2546,'tooltip':'Cayman Islands English Cayman Creole'},
         {'lat':18.7357,'lon':-70.1627,'tooltip':'Dominican Republic Spanish Haitian Creole'},
         {'lat':18.9712	,'lon':-72.2852,'tooltip':'Haiti French Haitian Creole'},
         {'lat':18.1096,'lon':-77.2975,'tooltip':'Jamaica English Jamaican Patois'},
         {'lat':18.2208,'lon':-66.5901,'tooltip':'Puert Rico (Haitian Creole Minority Population)'},
         {'lat':21.5218,'lon':-77.7812,'tooltip':'Cuba (Haitian Creole Minority Population)'}
-    ]
+    ]"""
 
     return render_template("index.html", markers=location_data)
+
+"""def testing():
+    f = open("locations.geojson")
+    location_data = json.load(f)
+
+    print(location_data)
+testing()"""
