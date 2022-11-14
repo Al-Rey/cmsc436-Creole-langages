2c55cc8e16dc67f4deb007238de53ff182dd3103
Jeremys11 <jeremysingh116@gmail.com>
2022-11-14 17:23:24 -0500

    Creole to English skeleton

---
+++ read_data.py
@@ -0,0 +1,57 @@
+import json
+import csv
+
+##  Global variables to define creole dictionary location and coordinates
+CREOLE_LOCATIONS = {"Haitian Creole": [[-72.285,18.9712],[-70.1627,18.7357]], "Jamaican Creole": [[-77.2975,18.1096]], "cayman_creole": [[-81.2546,19.3133]]}
+CREOLE_DICTIONARY_LIST = {"Haitian Creole":"scraping_scripts/hatian_creole_dictionary_v3.csv","Jamaican Creole":"scraping_scripts/Jamaican Creole.csv"}
+CREOLE_LIST = ["Haitian Creole","Jamaican Creole"]
+WORD_NOT_FOUND = "WORD_NOT_FOUND"
+
+#Reading database to find if word appears in creole
+def read_database(word,creole):
+    with open(CREOLE_DICTIONARY_LIST[creole],'r',newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['word'].rstrip() == word:
+                return (row['creole_word'])
+    return(WORD_NOT_FOUND)
+
+
+#Reading database to find which creole languages said creole word appears in
+def read_creole(word):
+    return
+
+#Returns makers if input is creole -> english
+def creole_markers(word):
+    return
+
+
+#Returns makers if input is english -> creole
+def english_markers(word):
+    creole_word = {}
+    for creole in CREOLE_LIST:
+        temp = read_database(word,creole)
+        if temp != WORD_NOT_FOUND:
+            creole_word[creole] = temp
+
+    #Creating Python Dictionary in form of geojson to be converted to json later
+    creole_dict = {"type":"FeatureCollection","features":[]}
+    for creole in creole_word:
+        for latlon in CREOLE_LOCATIONS[creole]:
+            creole_dict["features"].append(
+                {
+                "type":"Feature",
+                "properties":{"creole_language": creole, "word": word, "creole_word":creole_word[creole]},
+                "geometry":{"coordinates":latlon,"type":"Point"}
+                })
+
+    #Converting Python Dictionary to Json
+    creole_json = json.dumps(creole_dict)
+
+    #Saving Json to locations.json to be read by index.html
+    with open("locations.json", "w") as outfile:
+        outfile.write(creole_json)
+    f = open("locations.json")
+    markers = json.load(f)
+
+    return(markers)
--- website.py
+++ website.py
@@ -1,60 +1,29 @@
 from flask import Flask, render_template,request
-import json
-import csv
+from read_data import *
 
 app = Flask(__name__)
 
-##  Global variables to define creole dictionary location and coordinates
-##,[-70.1627,18.7357]
-CREOLE_LOCATIONS = {"Haitian Creole": [[-72.285,18.9712],[-70.1627,18.7357]], "Jamaican Creole": [[-77.2975,18.1096]], "cayman_creole": [[-81.2546,19.3133]]}
-CREOLE_DICTIONARY_LIST = {"Haitian Creole":"scraping_scripts/hatian_creole_dictionary_v3.csv","Jamaican Creole":"scraping_scripts/Jamaican Creole.csv"}
-CREOLE_LIST = ["Haitian Creole","Jamaican Creole"]
-WORD_NOT_FOUND = "WORD_NOT_FOUND"
-
-#Reading database to find if word appears in creole
-def read_database(word,creole):
-    with open(CREOLE_DICTIONARY_LIST[creole],'r',newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if row['word'].rstrip() == word:
-                return (row['creole_word'])
-    return(WORD_NOT_FOUND)
-
 #User word/phrase input
-@app.route('/')
+@app.route('/', methods=['GET'])
 def input():
     return render_template("input.html")
+
 
 # Interactive map
 @app.route('/index', methods = ['POST', 'GET'])
 def root():
 
     #Getting user input to query database
-    word = request.form["Word/Phrase"]
-    creole_word = {}
-    for creole in CREOLE_LIST:
-        temp = read_database(word,creole)
-        if temp != WORD_NOT_FOUND:
-            creole_word[creole] = temp
+    english_word = request.form.get('English')
+    creole_word = request.form.get('Creole')
 
-    #Creating Python Dictionary in form of geojson to be converted to json later
-    creole_dict = {"type":"FeatureCollection","features":[]}
-    for creole in creole_word:
-        for latlon in CREOLE_LOCATIONS[creole]:
-            creole_dict["features"].append(
-                {
-                "type":"Feature",
-                "properties":{"creole_language": creole, "word": word, "creole_word":creole_word[creole]},
-                "geometry":{"coordinates":latlon,"type":"Point"}
-                })
+    word = ""
+    if english_word != None:
+        word = english_word
+        markers = english_markers(word)
+    else:
+        word = creole_word
+        markers = english_markers(word)
 
-    #Converting Python Dictionary to Json
-    creole_json = json.dumps(creole_dict)
-
-    #Saving Json to locations.json to be read by index.html
-    with open("locations.json", "w") as outfile:
-        outfile.write(creole_json)
-    f = open("locations.json")
-    markers = json.load(f)
 
     return render_template("index.html",markers=markers)
