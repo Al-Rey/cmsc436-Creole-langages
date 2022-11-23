8f3bbbf82a719cb8f1142f3d998c7f85da41b45e
Al-Rey <45823889+Al-Rey@users.noreply.github.com>
2022-11-23 13:10:40 -0500

    wrote the script to generate n-grams and plot the frequency of the most and least common ngrams in bar charts. The plots generated were also saved in the same folder as the script. The plots generated so far are for bigramas and trigrams

--- vis_creation/generate.py
+++ vis_creation/generate.py
@@ -1,53 +1,113 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 
-def get_en_letter_freq(words):
-    freq = [0] * 26
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    total_letter_count = 0
+from nltk.util import ngrams
+from matplotlib.figure import  Figure
 
-    ignore=False
-    for word in words:
-        if pd.isnull(word):
-            continue
-
-        for ch in word:
-            if not ignore and ch == '(':
-                ignore = True
-            elif ignore and ch == ')':
-                ignore = False
-            elif not ignore:
-                index = alphabet.find(ch)
-
-                if index != -1:
-                    freq[index] += 1
-            total_letter_count += 1
+"""
+Name: create_ngrams
+Desc: This function creates n-grams. The user can speciy the number n that will be created (for example, 
+if the number 2 is passed, then the function will make bigrams). This function will then count how
+many times that bigram occurs else where in our dictionary.
+Return: The dictionary where the key is the bigrams and the values are the number of times the
+bigrams come up.
+"""
+def create_ngrams(text, n=1):
+    blacklist = [" ", ";", ",", "", "-", "!", "/", "."] # this is a list of characters to ignore for bigrams
+    frequency_dic = {} # the frequency of the n-grams that occur. The n-grams are the keys and the values are the count
     
-    percent_freq = [(x / total_letter_count) * 100 for x in freq]
+    for word in text: # loop through all the words present in the 
+        if word == np.nan:
+            continue
+        temp = re.sub(r"\(.*\)", "", word)
+        letters = list(temp)
+        tokens = [token.lower() for token in letters if token not in blacklist]
+        
+        grams = list(ngrams(tokens, n))
+        for gram in grams:
+            if gram in frequency_dic.keys():
+                frequency_dic[gram] += 1
+            else:
+                frequency_dic[gram] = 1
+        
+    return frequency_dic
 
-    return percent_freq
+"""
+Name: plot_ngrams
+Params:
+    data_dict - the dictionary with the ngrams and thier frequencies
+    creole_name - the name of the creole that the dictionary is from
+    graph_color - what color to make the bar graphs
+    n - the number of tokens in the ngram
+Desc: This function creates the bar charts showing the top 10 most frequent
+and least frequent ngrams for the given creole language. It then saves the figure
+as a png in the vis_creation folder
+Return: None
+"""
+def plot_ngrams(data_dict, creole_name, graph_color, n):
+    # sort the dictionary so the ngrams are in order by letter combination
+    sorted_dic = sorted(data_dict.items(), key=lambda x:x[1], reverse=True)
+    labels = list(map(str, [item[0] for item in sorted_dic])) # get the ngram names
+    combo_freq = [item[1] for item in sorted_dic] # get the frequency data
 
+    # get the 10 highest and 10 lowest ngrams
+    top_10_values =  combo_freq[:10]
+    top_10_labels = labels[:10]
+    bottom_10_values = combo_freq[-1:-11:-1]
+    bottom_10_labels = labels[-1:-11:-1]
 
+    # get the title of the graphs and the name of the image file the graphs will be saved to
+    title1 = ""
+    title2 = ""
+    file_name = ""
+    if n == 1:
+        title1 = creole_name + " top 10 most common unigrams"
+        title2 = creole_name + " top 10 least common unigrams"
+        file_name = creole_name + "_unigram_frequency_graphs.png"
+    elif n == 2:
+        title1 = creole_name + " top 10 most common bigrams"
+        title2 = creole_name + " top 10 least common bigrams"
+        file_name = creole_name + "_bigram_frequency_graphs.png"
+    elif n == 3:
+        title1 = creole_name + " top 10 most common trigrams"
+        title2 = creole_name + " top 10 least common trigrams"
+        file_name = creole_name + "_trigram_frequency_graphs.png"
+    else:
+        title1 = creole_name + " top 10 most common " + str(n) + "-grams"
+        title2 = creole_name + " top 10 least common " + str(n) + "-grams"
+        file_name = creole_name + "_" + str(n) + "gram_frequency_graphs.png"
+    
+    # print(bottom_10_labels)
+    
+    # make the plot with two graphs on it
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=True) # make the plot with two graphs on it
+    f.set(figheight=120, figwidth=80)
+    f.subplots_adjust(bottom=0.2, top=0.9)
 
-
-def plot_letter_frequencies(data_df):
-    labels = data_df["langauge"].values
-    letters = list("abcdefghijklmnopqrstuvwxyz")
-    print(data_df.head())
-    bar_width = 0.25
-    x_axis = np.arange(26)
-    counter = 0
-    for index, row in data_df.iterrows():
-        plt.bar(x_axis + bar_width * counter, row["en_char_freq"], label=row["langauge"], width=bar_width, edgecolor="black")
-        counter += 1
-
-    plt.xticks(x_axis + bar_width, letters)
-    plt.xlabel("English letters")
-    plt.ylabel("Percent of letter occurances")
-    plt.title("Frequency of each english character occuring per language")
-    plt.legend()
+    # make the plot for the 10 most common ngrams
+    ax1.bar(top_10_labels, top_10_values, color=graph_color)
+    ax1.set_title(title1)
+    
+    ax1.set_ylabel("frequency")
+    ax1.tick_params("x", labelrotation=90)
+    ax1.set_xlabel("Letter Combination")
+    for i in range(10): # put the frequencies for each ngram above the bars
+        ax1.text(i, top_10_values[i] + 1, top_10_values[i], ha = 'center')
+    
+    # make the plot for the 10 least common ngrams
+    ax2.bar(bottom_10_labels, bottom_10_values, color=graph_color)
+    for i in range(10):
+        ax2.text(i, bottom_10_values[i] + 1, bottom_10_values[i], ha = 'center')
+    ax2.set_xlabel("Letter Combination")
+    ax2.tick_params("x", labelrotation=90)
+    ax2.set_title(title2)
+    
+    # show the plot and save the viz
     plt.show()
+    f.savefig("vis_Creation/"+file_name)
+
 
 if __name__ == '__main__':
     # load our data frames
@@ -60,12 +120,24 @@ if __name__ == '__main__':
     haitian_df.drop(columns="creole name", inplace=True)
     louisiana_df.drop(columns="creole name", inplace=True)
 
-    # the dataframe with data for the languages themselves
-    langauge_df = pd.DataFrame(columns=["langauge", "en_char_freq"])
-
-    langauge_df.loc[len(langauge_df.index), :] = ["louisiana creole", get_en_letter_freq(louisiana_df["creole_word"])]
-    langauge_df.loc[len(langauge_df.index), :] = ["jamaican creole", get_en_letter_freq(jamaica_df["Words"])]
-    langauge_df.loc[len(langauge_df.index), :] = ["haitian creole", get_en_letter_freq(haitian_df["creole_word"])]
+    # drop any NaN values from the dataframe
+    louisiana_df.dropna(inplace=True)
+    haitian_df.dropna(inplace=True)
+    jamaica_df.dropna(inplace=True)
+   
+    # get the haitian words from each dataframe
+    words_louisiana = louisiana_df.loc[:, "creole_word"].tolist()
+    words_haitian = haitian_df.loc[:, "word"].tolist()
+    words_jamaican = jamaica_df.loc[:, "Words"].tolist()
     
-    print(langauge_df.head())
-    plot_letter_frequencies(langauge_df)
+    # get the bigram data
+    n = 3
+    louisiana_bigrams = create_ngrams(words_louisiana[:], n) 
+    haitian_bigrams = create_ngrams(words_haitian[:], n) 
+    jamaican_bigrams = create_ngrams(words_jamaican[:], n) 
+
+
+    # plot the ngrams
+    plot_ngrams(louisiana_bigrams, "Louisiana Creole", "maroon", n)
+    plot_ngrams(haitian_bigrams, "Haitian Creole", "green", n)
+    plot_ngrams(jamaican_bigrams, "Jamaican Creole", "blue", n)
